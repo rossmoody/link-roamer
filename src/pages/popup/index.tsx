199c3dd0750101a39d77dff26d31ffa64a07d78f
rossmoody <fiftyfivehis@gmail.com>
2022-03-28 21:52:55 -0700

    format

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 
 const root = document.querySelector('#root')
 
-render(<App />, root)
+ReactDOM.render(<App />, root)
--- src/scripts/get-active-tab.ts
+++ src/scripts/get-active-tab.ts
@@ -1,3 +1,6 @@
+/**
+ * Query the current active tab and return it for use in executing scripts.
+ */
 export async function getActiveTab() {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
   return tabs[0]
