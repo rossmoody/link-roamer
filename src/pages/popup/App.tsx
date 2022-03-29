65ea7ae6122006e4b4740ba6968682d5d5c2c4be
rossmoody <fiftyfivehis@gmail.com>
2022-03-28 21:13:40 -0700

    it's working

--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 
-const App = (props: unknown) => {
-  console.log(props)
-  
+const App = () => {
+  const handleClick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting
+        .executeScript({
+          target: { tabId: tabs[0].id ?? 0 },
+          func: () => {
+            const linkElements = Array.from(document.querySelectorAll('a'))
+            const data = linkElements.map((element) => element.href)
+            console.log(data)
+          },
+        })
+        .then()
+    })
+  }
+
   return (
     <div>
       <h1>Link Grabber</h1>
       <p>If you are seeing this, React is working!</p>
+      <button onClick={handleClick}>Click</button>
     </div>
   )
 }
